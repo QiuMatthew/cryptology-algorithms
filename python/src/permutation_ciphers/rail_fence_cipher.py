460dcac6824b1d4416ad39b510ac903286f890de
QiuMatthew <uzenkyu@gmail.com>
2024-07-26 14:22:51 +0900

    fix bug and add test: rail fence cipher

--- python/src/permutation_ciphers/rail_fence_cipher.py
+++ python/src/permutation_ciphers/rail_fence_cipher.py
@@ -1,6 +1,19 @@
+from sys import get_int_max_str_digits
+
+
 class RailFenceCipher:
     def __init__(self, row_count):
         self.row_count = row_count
+        self.col_count = 0
+
+    def row_col_to_index(self, row, col):
+        col_offset = (self.row_count - 1) * col + 1
+        if col % 2 == 0:
+            row_offset = row - 1
+        else:
+            row_offset = self.row_count - row - 2
+        return col_offset + row_offset
+        
 
     def encrypt(self, plaintext):
         # corner case
@@ -8,7 +21,8 @@ class RailFenceCipher:
             return plaintext
 
         # for n row and m column, we have (m - 1)(n - 1) + 1 <= len(plaintext) < m(n - 1) + 1
-        self.col_count = (len(plaintext) - 1) // (self.row_count - 1)
+        self.col_count = (len(plaintext) - 2) // (self.row_count - 1) + 1
+        print(f"letter_count = {len(plaintext)}, col_count = {self.col_count}")
         table = [["\0" for _ in range(self.col_count)] for _ in range(self.row_count)]
 
         for i, char in enumerate(plaintext):
@@ -21,7 +35,10 @@ class RailFenceCipher:
                 row_index = row_offset + 1
             else:
                 row_index = self.row_count - row_offset - 2
+            # print(row_index, col_index)
             table[row_index][col_index] = char
+
+        # print(table)
 
         ciphertext = ""
         for row in table:
@@ -36,7 +53,7 @@ class RailFenceCipher:
         if self.row_count == 1:
             return ciphertext
 
-        self.col_count = (len(ciphertext) - 1) // (self.row_count - 1)
+        self.col_count = (len(ciphertext) - 2) // (self.row_count - 1) + 1
         table = [["\0" for _ in range(self.col_count)] for _ in range(self.row_count)]
         
         table[0][0] = ciphertext[0]
@@ -44,7 +61,11 @@ class RailFenceCipher:
         while row < self.row_count:
             # first row
             if row == 0:
-                while col < self.col_count:
+                while col < self.col_count - 1:
+                    table[row][col] = ciphertext[i]
+                    col += 2
+                    i += 1
+                if col == self.col_count - 1 and self.row_col_to_index(row, col) <= len(ciphertext) - 1:
                     table[row][col] = ciphertext[i]
                     col += 2
                     i += 1
@@ -63,13 +84,18 @@ class RailFenceCipher:
                 # print("last row done")
             # rows in the middle
             else:
-                while col < self.col_count:
+                while col < self.col_count - 1:
+                    table[row][col] = ciphertext[i]
+                    col += 1
+                    i += 1
+                if col == self.col_count - 1 and self.row_col_to_index(row, col) <= len(ciphertext) - 1:
                     table[row][col] = ciphertext[i]
                     col += 1
                     i += 1
                 col = 0
                 row += 1
                 # print(f"No.{row + 1} row done")
+        # print(table)
         
         plaintext = table[0][0]
         for col in range(self.col_count):
@@ -78,11 +104,14 @@ class RailFenceCipher:
                     plaintext += table[row_offset + 1][col]
                 else:
                     plaintext += table[self.row_count - row_offset - 2][col]
+                if len(plaintext) == len(ciphertext):
+                    return plaintext
 
+        # NOTE: Maybe it's better to raise an error
         return plaintext
 
 if __name__ == "__main__":
-    row_count = 2
+    row_count = 5
     cipher = RailFenceCipher(row_count)
 
     plaintext = "meetmeafterthetogaparty"
@@ -92,3 +121,12 @@ if __name__ == "__main__":
     decrypted = cipher.decrypt(encrypted)
     print(f"Decrypted: {decrypted}")
 
+    row_count = 5
+    cipher = RailFenceCipher(row_count)
+
+    plaintext = "wearediscoveredsaveyourself"
+    encrypted = cipher.encrypt(plaintext)
+    print(f"Encrypted: {encrypted}")
+
+    decrypted = cipher.decrypt(encrypted)
+    print(f"Decrypted: {decrypted}")
---
+++ python/tests/permutation_ciphers/test_rail_fence_cipher.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import pytest
+
+# Add the src directory to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
+
+from permutation_ciphers.rail_fence_cipher import RailFenceCipher
+
+def test_rail_fence_cipher():
+    row_count = 2
+    cipher = RailFenceCipher(row_count)
+    plaintext = "meetmeafterthetogaparty"
+    ciphertext = cipher.encrypt(plaintext)
+    assert(cipher.decrypt(ciphertext) == plaintext)
+
+    row_count = 5
+    cipher = RailFenceCipher(row_count)
+    plaintext = "wearediscoveredsaveyourself"
+    ciphertext = cipher.encrypt(plaintext)
+    assert(cipher.decrypt(ciphertext) == plaintext)
+
+if __name__ == "__main__":
+    pytest.main()
