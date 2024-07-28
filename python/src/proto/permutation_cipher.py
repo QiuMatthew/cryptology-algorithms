ff6abc67a2b9d1992e73ac0a43f445f3441b4c9f
QiuMatthew <uzenkyu@gmail.com>
2024-07-29 01:32:46 +0900

    update permutation cipher proto

--- python/src/proto/permutation_cipher.py
+++ python/src/proto/permutation_cipher.py
@@ -1,5 +1,19 @@
 from .base_cipher import BaseCipher
 
 class PermutationCipher(BaseCipher):
-    def __init__(self, key) -> None:
-        super().__init__(key)
+    '''
+    We use lowercase letters for plaintext and uppercase letters for ciphertext
+    '''
+    def preprocess_plaintext(self, text) -> str:
+        '''
+        Remove spaces, numbers and other non-alphabetic symbols
+        Convert uppercase letters to lowercase
+        '''
+        return "".join(char.lower() for char in text if char.isalpha())
+
+    def preprocess_ciphertext(self, text) -> str:
+        '''
+        Remove spaces, numbers and other non-alphabetic symbols
+        Convert lowercase letters to uppercase
+        '''
+        return "".join(char.upper() for char in text if char.isalpha())
