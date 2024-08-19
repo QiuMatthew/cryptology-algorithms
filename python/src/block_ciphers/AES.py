a62b9e994a658c5ece2bc33c5f240750627ec55d
QiuMatthew <uzenkyu@gmail.com>
2024-08-20 01:30:17 +0900

    new algorithm: AES (added decryption)

--- python/src/block_ciphers/AES.py
+++ python/src/block_ciphers/AES.py
@@ -22,6 +22,24 @@ class AESCipher(BlockCipher):
         [0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf],
         [0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16]
     ]
+    INV_S_BOX = [
+        [0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb],
+        [0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb],
+        [0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e],
+        [0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2, 0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25],
+        [0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92],
+        [0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda, 0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84],
+        [0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a, 0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06],
+        [0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02, 0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b],
+        [0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea, 0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73],
+        [0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85, 0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e],
+        [0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89, 0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b],
+        [0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20, 0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4],
+        [0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f],
+        [0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef],
+        [0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61],
+        [0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d]
+    ]
 
     def __init__(self, key) -> None:
         self.key = [ord(c) for c in key]
@@ -72,10 +90,22 @@ class AESCipher(BlockCipher):
                 state[row][col] = self.S_BOX[state[row][col] >> 4][state[row][col] & 0x0F]
         return state
 
+    def inv_substitute_bytes(self, state: list[list[int]]) -> list[list[int]]:
+        for row in range(4):
+            for col in range(4):
+                state[row][col] = self.INV_S_BOX[state[row][col] >> 4][state[row][col] & 0x0F]
+        return state
+
     def shift_rows(self, state: list[list[int]]) -> list[list[int]]:
         state[1][0], state[1][1], state[1][2], state[1][3] = state[1][1], state[1][2], state[1][3], state[1][0]
         state[2][0], state[2][1], state[2][2], state[2][3] = state[2][2], state[2][3], state[2][0], state[2][1]
         state[3][0], state[3][1], state[3][2], state[3][3] = state[3][3], state[3][0], state[3][1], state[3][2]
+        return state
+
+    def inv_shift_rows(self, state: list[list[int]]) -> list[list[int]]:
+        state[1][0], state[1][1], state[1][2], state[1][3] = state[1][3], state[1][0], state[1][1], state[1][2]
+        state[2][0], state[2][1], state[2][2], state[2][3] = state[2][2], state[2][3], state[2][0], state[2][1]
+        state[3][0], state[3][1], state[3][2], state[3][3] = state[3][1], state[3][2], state[3][3], state[3][0]
         return state
 
     def galois_field_mul(self, a, b):
@@ -103,6 +133,15 @@ class AESCipher(BlockCipher):
             state[3][col] = self.galois_field_mul(a[0], 3) ^ a[1] ^ a[2] ^ self.galois_field_mul(a[3], 2)
         return state
 
+    def inv_mix_columns(self, state: list[list[int]]) -> list[list[int]]:
+        for col in range(4):
+            a = [state[row][col] for row in range(4)]
+            state[0][col] = self.galois_field_mul(a[0], 0x0e) ^ self.galois_field_mul(a[1], 0x0b) ^ self.galois_field_mul(a[2], 0x0d) ^ self.galois_field_mul(a[3], 0x09)
+            state[1][col] = self.galois_field_mul(a[0], 0x09) ^ self.galois_field_mul(a[1], 0x0e) ^ self.galois_field_mul(a[2], 0x0b) ^ self.galois_field_mul(a[3], 0x0d)
+            state[2][col] = self.galois_field_mul(a[0], 0x0d) ^ self.galois_field_mul(a[1], 0x09) ^ self.galois_field_mul(a[2], 0x0e) ^ self.galois_field_mul(a[3], 0x0b)
+            state[3][col] = self.galois_field_mul(a[0], 0x0b) ^ self.galois_field_mul(a[1], 0x0d) ^ self.galois_field_mul(a[2], 0x09) ^ self.galois_field_mul(a[3], 0x0e)
+        return state
+
     def matrix_to_matrix_encrypt(self, state: list[list[int]]) -> list[list[int]]:
         # Add encryption key
         state = self.add_round_key(state, 0)
@@ -121,7 +160,27 @@ class AESCipher(BlockCipher):
         state = self.add_round_key(state, 10)
         return state
 
+    def matrix_to_matrix_decrypt(self, state: list[list[int]]) -> list[list[int]]:
+        # Inverse of last round, without mix column
+        state = self.add_round_key(state, 10)
+        state = self.inv_shift_rows(state)
+        state = self.inv_substitute_bytes(state)
+        # Inverse of nine rounds of process
+        for round in range(9, 0, -1):
+            state = self.add_round_key(state, round)
+            state = self.inv_mix_columns(state)
+            state = self.inv_shift_rows(state)
+            state = self.inv_substitute_bytes(state)
+        # Add encryption key, the "addition" here is defined as bitwise xor
+        state = self.add_round_key(state, 0)
+        return state
+
     def encrypt(self, plaintext: str) -> str:
+        """
+        The AES-128 encryption process
+        Input: plaintext string with length of 16 (16 bytes)
+        Output: hexadecimal ciphertext string with length of 32 (16 bytes)
+        """
         assert len(plaintext) == 16, "Plaintext must be 16 bytes long."
         plaintext_bytes = [ord(c) for c in plaintext]
         # print(f"plaintext_bytes: {plaintext_bytes}")
@@ -136,7 +195,23 @@ class AESCipher(BlockCipher):
         return ciphertext_hex
 
     def decrypt(self, ciphertext: str) -> str:
-        return super().decrypt(ciphertext)
+        """
+        The AES-128 decryption process
+        Input: hexadecimal ciphertext string with length of 32 (16 bytes)
+        Output: plaintext string with length of 16 (16 bytes)
+        """
+        assert len(ciphertext) == 32, "Ciphertext must be 16 bytes long."
+        # print(f"ciphertext_hex = {ciphertext}")
+        ciphertext_bytes = self._hex_to_bytes(ciphertext)
+        # print(f"ciphertext_bytes: {ciphertext_bytes}")
+        ciphertext_matrix = self._bytes_to_matrix(ciphertext_bytes)
+        # print(f"ciphertext_matrix: {ciphertext_matrix}")
+        plaintext_matrix = self.matrix_to_matrix_decrypt(ciphertext_matrix)
+        # print(f"plaintext_matrix: {plaintext_matrix}")
+        plaintext_bytes = self._matrix_to_bytes(plaintext_matrix)
+        # print(f"plaintext_bytes: {plaintext_bytes}")
+        plaintext = "".join(chr(byte) for byte in plaintext_bytes)
+        return plaintext
 
     def _xor_words(self, word1: list[int], word2: list[int]):
         """
@@ -172,13 +247,20 @@ class AESCipher(BlockCipher):
         byte_list = [state[row][col] for col in range(4) for row in range(4)]
         return byte_list
 
-    def _bytes_to_hex(self, byte_list) -> str:
+    def _bytes_to_hex(self, byte_list: list[int]) -> str:
         return ''.join(f'{byte:02x}' for byte in byte_list)
+
+    def _hex_to_bytes(self, hex_str: str) -> list[int]:
+        return [int(hex_str[i:i+2], 16) for i in range(0, len(hex_str), 2)]
 
 if __name__ == "__main__":
     key = "thisisakey123456"
     cipher = AESCipher(key)
     plaintext = "thisisplaintext1"
     
-    ciphertext_hex = cipher.encrypt(plaintext)
-    print(ciphertext_hex)
+    encrypted = cipher.encrypt(plaintext)
+    decrypted = cipher.decrypt(encrypted)
+
+    print(f"Plaintext: {plaintext}")
+    print(f"Encrypted: {encrypted}")
+    print(f"Decrypted: {decrypted}")
